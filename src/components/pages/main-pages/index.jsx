aeb3afe40ecf0a5a7e6981bfcda66f0b1e41c34b
niloofarssohrabi <nilufar.sohrabi@gmail.com>
2021-02-19 13:49:02 +0330

    header

---
+++ src/components/pages/main-pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header } from '../../layout/fixed-layout/header'
+
+export const MainPage = () => {
+    return (
+        <div>
+            
+            <Header/>
+        </div>
+    )
+}
