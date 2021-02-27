277ba337543cf18d28fc3164937416655af4506c
niloofarssohrabi <nilufar.sohrabi@gmail.com>
2021-02-27 14:29:42 +0330

    edit

--- src/showInfoOfFetchData/showShortInfoOfPopularSlider/index.jsx
+++ src/showInfoOfFetchData/showShortInfoOfPopularSlider/index.jsx
@@ -25,21 +25,21 @@ const ShowShortInfoOfPopularSlider = () => {
             breakpoint: { max: 3000, min: 1024 },
             items: 3,
             paritialVisibilityGutter: 60
-          },
-          tablet: {
+        },
+        tablet: {
             breakpoint: { max: 1024, min: 464 },
             items: 2,
             paritialVisibilityGutter: 50
-          },
-          mobile: {
+        },
+        mobile: {
             breakpoint: { max: 464, min: 0 },
             items: 1,
             paritialVisibilityGutter: 30
-          }
-        };
+        }
+    };
     return (
-      
-        <Carousel 
+
+        <Carousel
             ssr
             partialVisbile
             responsive={responsive}>
@@ -47,38 +47,44 @@ const ShowShortInfoOfPopularSlider = () => {
                 resiveDataState ?
                     resiveDataState.map((item) => {
                         return (
-                            <div key={item.id}>
-                                {
-                                    mouseOverOnImage == item.id ?
-                                        <div className="box" >
-                                            <div className="playIcon">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="80" fill="currentColor" className="bi bi-play-circle" viewBox="0 0 16 16">
-                                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                                    <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
-                                                </svg>
-                                            </div>
-                                            <div className="infoIcon">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="30" fill="currentColor" className="bi bi-info-circle" viewBox="0 0 16 16">
-                                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                                                </svg>
-                                            </div>
+                            <div className="total">
 
-                                        </div> : null
+                                <div key={item.id}>
+                                    {
+                                        mouseOverOnImage == item.id ?
+                                            <div className="box" >
+                                                <div className="playIcon">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="80" fill="currentColor" className="bi bi-play-circle" viewBox="0 0 16 16">
+                                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                        <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+                                                    </svg>
+                                                </div>
+                                                <div className="infoIcon">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="30" fill="currentColor" className="bi bi-info-circle" viewBox="0 0 16 16">
+                                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                        <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                                                    </svg>
+                                                </div>
 
-                                }
-                                {/* ----------------------------------------------------- */}
-                                <img className="poster" src={item.poster}
-                                    onMouseEnter={() => handleMouseOverOnImage(item.id)}
-                                    onMouseLeave={() => handleMouseOverOnImage(item.id)} />
-                                <div className="titleMovie">{item.title}</div>
-                                <div className="yearOfMovie">
-                                    Production Year {item.year}
+                                            </div> : null
+
+                                    }
+                                    {/* ----------------------------------------------------- */}
+                                  
+                                  <img className="poster" src={item.poster}
+                                        onMouseEnter={() => handleMouseOverOnImage(item.id)}
+                                        onMouseLeave={() => handleMouseOverOnImage(item.id)} />
+                                    <div className="titleMovie">{item.title}</div>
+                                    <div className="yearOfMovie">
+                                        Production Year {item.year}
+                                    </div>
+                                    <div className="countryOfMovie">
+                                        {item.country}
+                                        </div>
+                                
+                                   
                                 </div>
-                                <div className="countryOfMovie">
-                                    {item.country}</div>
                             </div>
-
                         )
                     }) : <Loading />
             }
