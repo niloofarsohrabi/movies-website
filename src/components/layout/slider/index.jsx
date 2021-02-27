2ae95b481bfea705e3cf422c04f06f1c975cc44b
niloofarssohrabi <nilufar.sohrabi@gmail.com>
2021-02-27 13:32:39 +0330

    edit banner

--- src/components/layout/slider/index.jsx
+++ src/components/layout/slider/index.jsx
@@ -5,24 +5,29 @@ import styleSilder from './styleSilder.module.scss'
 export const Slider = () => {
     return (
         <>
-            <div className={styleSilder.c}></div>
+
             <div className="container-fluid">
-                <div className="row">
-                    
-                    <img className={styleSilder.imageSlider} src={sliderImage} />
-                    <div className="col-lg-12">
-                    <h1 className={styleSilder.title}>Welcome.</h1>
+                <div className={styleSilder.totalSlider}>
+                    <div className="row">
+
+                        <img className={styleSilder.imageSlider} src={sliderImage} />
+                        <div className="col-lg-12">
+                            <h1 className={styleSilder.title}>Welcome.</h1>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-12">
-                    <h2 className={styleSilder.subTitleSlider}>Millions of movies, TV shows and people to discover. Explore now.</h2>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <h2 className={styleSilder.subTitleSlider}>Millions of movies, TV shows and people to discover. Explore now.</h2>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-12">
-                    <input className={styleSilder.inputSlider} placeholder="Search for a movie,Tv show,Person..." />
-                    <button className={styleSilder.btnSearchSlider}>Search</button>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className={styleSilder.searchBox}>
+                                <input className={styleSilder.inputSlider} placeholder="Search for a movie,Tv show,Person..." />
+                                <button className={styleSilder.btnSearchSlider}>Search</button>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
