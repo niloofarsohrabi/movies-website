ad133ea64ae71386177e917da1a95cad4d4dd751
niloofarssohrabi <nilufar.sohrabi@gmail.com>
2021-03-09 22:40:13 +0330

    fix some error

--- src/components/pages/movieDetail-page/components/movieTopCast/index.jsx
+++ src/components/pages/movieDetail-page/components/movieTopCast/index.jsx
@@ -23,7 +23,7 @@ export const MovieTopCast = ({ movieId }) => {
                                 <img src={`${getfullImagePath.base_url}${getfullImagePath.logo_sizes[2]}${item.profile_path}`} />
                                 :
                                 <div className={styleTopCast.profileLogo}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="150" height="250" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="150" height="250" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
                                         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
                                     </svg>
                                 </div>
